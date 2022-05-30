2d1a2290ef4436528440c03f0f3a3545f58c0b5b
claud <claudiodemarzo.62@gmail.com>
2022-05-30 10:32:48 +0200

    Refactor + Aggiunta Documentazione

--- src/main/java/it/leonardo/leonardoapiboot/controller/RegioneController.java
+++ src/main/java/it/leonardo/leonardoapiboot/controller/RegioneController.java
@@ -1,6 +1,9 @@
 package it.leonardo.leonardoapiboot.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.leonardo.leonardoapiboot.entity.Regione;
 import it.leonardo.leonardoapiboot.service.RegioneService;
 import org.apache.commons.logging.Log;
@@ -16,7 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/regione")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"https://leonardostart.tk", "https://localhost"}, allowCredentials = "true")
 public class RegioneController {
 
     private final Log log = LogFactory.getLog(RegioneController.class);
@@ -25,39 +28,42 @@ public class RegioneController {
     private RegioneService service;
 
     @GetMapping
+    @Operation(description = "Ottiene una lista di tutte le Regioni presenti nel Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "La lista è stata popolata e viene restituita"),
+            @ApiResponse(responseCode = "204", description = "La richiesta è andata a buon fine, ma la lista risultante è vuota"),
+            @ApiResponse(responseCode = "500", description = "Errore generico del server")
+    })
     public ResponseEntity<List<Regione>> getAllRegione() {
         log.info("Invoked TagController.getAllRegione()");
-        ResponseEntity<List<Regione>> response;
 
         try {
             List<Regione> regioni = service.getAll();
-            if (regioni.isEmpty()) {
-                response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            } else {
-                response = new ResponseEntity<>(regioni, HttpStatus.OK);
-            }
+
+            if (regioni.isEmpty()) return ResponseEntity.noContent().build();
+            return ResponseEntity.ok(regioni);
         } catch (Exception e) {
-            response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.internalServerError().build();
         }
-        return response;
     }
 
     @GetMapping("{id}")
+    @Operation(description = "Ottiene una Regione il quale id è fornito")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Richiesta andata a buon fine, l'oggetto è restituito"),
+            @ApiResponse(responseCode = "404", description = "Richiesta andata a buon fine, ma l'oggetto non è stato trovato"),
+            @ApiResponse(responseCode = "500", description = "Errore generico del server")
+    })
     public ResponseEntity<Regione> getRegioneById(@PathVariable("id") Integer id) {
         log.info("Invoked TagController.getRegioneById(" + id + ")");
-        ResponseEntity<Regione> response;
 
         try {
             Optional<Regione> regione = service.getById(id);
-            if (regione.isPresent()) {
-                response = new ResponseEntity<>(regione.get(), HttpStatus.OK);
-            } else {
-                response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
 
-        return response;
+            if (!regione.isPresent()) return ResponseEntity.notFound().build();
+            return ResponseEntity.ok(regione.get());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
