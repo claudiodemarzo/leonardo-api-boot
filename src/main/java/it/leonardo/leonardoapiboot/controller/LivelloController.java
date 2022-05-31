ca1849ef99e04f77d499e0020d93949d7147e69e
claud <claudiodemarzo.62@gmail.com>
2022-05-31 14:14:06 +0200

    Refactor + Aggiunta Documentazione

--- src/main/java/it/leonardo/leonardoapiboot/controller/LivelloController.java
+++ src/main/java/it/leonardo/leonardoapiboot/controller/LivelloController.java
@@ -1,10 +1,14 @@
 package it.leonardo.leonardoapiboot.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.leonardo.leonardoapiboot.entity.Livello;
 import it.leonardo.leonardoapiboot.service.LivelloService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,34 +28,41 @@ public class LivelloController {
     private LivelloService service;
 
     @GetMapping
+    @Operation(description = "Restituisce una lista di tutti i livelli raggiungibili.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Richiesta andata a buon fine. La lista è popolata e restituita"),
+            @ApiResponse(responseCode = "204", description = "Richiesta andata a buon fine, ma la lista è vuota"),
+            @ApiResponse(responseCode = "500", description = "Errore generico del server")
+    })
     public ResponseEntity<List<Livello>> getAll(){
         log.info("Invoked LivelloController.getAll()");
-        ResponseEntity<List<Livello>> resp = null;
 
         try{
             List<Livello> lst = service.getAll();
-            if(!lst.isEmpty()) resp = new ResponseEntity<>(lst, HttpStatus.OK);
-            else resp = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            if(lst.isEmpty()) return ResponseEntity.noContent().build();
+            return ResponseEntity.ok(lst);
         }catch(Exception e){
-            resp = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.internalServerError().build();
         }
-
-        return resp;
     }
 
     @GetMapping("{id}")
+    @Operation(description = "Restituisce l'oggetto relativo al livello il quale id è fornito")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Richiesta andata a buon fine, l'oggetto è restituito"),
+            @ApiResponse(responseCode = "404", description = "Non è stato possibile trovare l'oggetto associato all'id fornito"),
+            @ApiResponse(responseCode = "500", description = "Errore generico del server")
+    })
     public ResponseEntity<Livello> getById(@PathVariable("id") Integer id){
         log.info("Invoked LivelloController.getById(" + id + ")");
-        ResponseEntity<Livello> resp = null;
 
         try{
             Optional<Livello> lvl = service.getById(id);
-            if(lvl.isPresent()) resp = new ResponseEntity<>(lvl.get(), HttpStatus.OK);
-            else resp = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            if(!lvl.isPresent()) return ResponseEntity.notFound().build();
+            return ResponseEntity.ok(lvl.get());
         }catch(Exception e){
-            resp = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.internalServerError().build();
         }
 
-        return resp;
     }
 }
