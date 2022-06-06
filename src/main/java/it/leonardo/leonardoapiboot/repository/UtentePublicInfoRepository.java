dabe2ad6563a5060a9a9c97c5b0ecbc26ff0e47a
claud <claudiodemarzo.62@gmail.com>
2022-06-06 08:45:17 +0200

    Aggiunta Repository per tabella utenti_public_information

---
+++ src/main/java/it/leonardo/leonardoapiboot/repository/UtentePublicInfoRepository.java
@@ -0,0 +1,7 @@
+package it.leonardo.leonardoapiboot.repository;
+
+import it.leonardo.leonardoapiboot.entity.UtentePublicInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtentePublicInfoRepository extends JpaRepository<UtentePublicInfo, Integer> {
+}
