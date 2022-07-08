88e42dcdb054ec2662ca0359f63a7c6daeb8efeb
claud <claudiodemarzo.62@gmail.com>
2022-07-08 16:40:43 +0200

    Cambiata Struttura ID TagsUtente. Sistemati metodi TagController

--- src/main/java/it/leonardo/leonardoapiboot/repository/TagsUtenteRepository.java
+++ src/main/java/it/leonardo/leonardoapiboot/repository/TagsUtenteRepository.java
@@ -1,12 +1,14 @@
 package it.leonardo.leonardoapiboot.repository;
 
 import it.leonardo.leonardoapiboot.entity.TagsUtente;
-import it.leonardo.leonardoapiboot.entity.TagsUtenteKey;
+import it.leonardo.leonardoapiboot.entity.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TagsUtenteRepository extends JpaRepository<TagsUtente, TagsUtenteKey> {
+public interface TagsUtenteRepository extends JpaRepository<TagsUtente, TagsUtente.TagsUtenteKey> {
     List<TagsUtente> findById_Utente(Integer utente);
+
+    List<TagsUtente> findTagsUtenteByUtente(Utente u);
 
 }
--- src/main/java/it/leonardo/leonardoapiboot/service/impl/TagsUtenteServiceImpl.java
+++ src/main/java/it/leonardo/leonardoapiboot/service/impl/TagsUtenteServiceImpl.java
@@ -1,12 +1,16 @@
 package it.leonardo.leonardoapiboot.service.impl;
 
+import it.leonardo.leonardoapiboot.entity.Tag;
 import it.leonardo.leonardoapiboot.entity.TagsUtente;
+import it.leonardo.leonardoapiboot.entity.Utente;
 import it.leonardo.leonardoapiboot.repository.TagsUtenteRepository;
 import it.leonardo.leonardoapiboot.service.TagsUtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TagsUtenteServiceImpl implements TagsUtenteService {
@@ -14,8 +18,22 @@ public class TagsUtenteServiceImpl implements TagsUtenteService {
     @Autowired
     TagsUtenteRepository repo;
 
+
     @Override
-    public List<TagsUtente> getTagsByUserId(Integer userID) {
-        return repo.findById_Utente(userID);
+    public List<Tag> getTagsByUtente(Utente u) {
+        List<TagsUtente> tagsUtente = repo.findTagsUtenteByUtente(u);
+        List<Tag> lst = new ArrayList<>();
+        tagsUtente.forEach(tu -> lst.add(tu.getTag()));
+        return lst;
+    }
+
+    @Override
+    public Optional<TagsUtente> getTagsUtenteById(TagsUtente.TagsUtenteKey id) {
+        return repo.findById(id);
+    }
+
+    @Override
+    public List<TagsUtente> getTagsUtenteByUtente(Utente u) {
+        return repo.findTagsUtenteByUtente(u);
     }
 }
