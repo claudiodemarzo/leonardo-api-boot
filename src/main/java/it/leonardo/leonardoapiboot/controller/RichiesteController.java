e9b0798eb95d85066c8a574025e1f61d5bb85f9b
claud <claudiodemarzo.62@gmail.com>
2022-11-14 17:04:54 +0100

    aggiustato mapping del RichiesteController

--- src/main/java/it/leonardo/leonardoapiboot/controller/RichiesteController.java
+++ src/main/java/it/leonardo/leonardoapiboot/controller/RichiesteController.java
@@ -10,15 +10,13 @@ import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
-@RestController("/richieste")
+@RestController
+@RequestMapping("/richieste")
 public class RichiesteController {
 
     private final Log log = LogFactory.getLog(RichiesteController.class);
